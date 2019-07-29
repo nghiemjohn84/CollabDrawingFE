9dd91fc65aa077f7f10b8fdc1c2b1f14291cedc8
sammoran@gmail.com <sammoran@gmail.com>
2019-07-29 15:12:11 +0100

    basic painting methods handled

--- app/src/main/java/com/example/collabdrawingfe/PaintView.java
+++ app/src/main/java/com/example/collabdrawingfe/PaintView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.*;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class PaintView extends View {
     public static final int DEFAULT_COLOR = Color.BLACK;
     public static final int DEFAULT_BG_COLOR = Color.WHITE;
     private static final float TOUCH_TOLERANCE = 4;
-    private float userX, userX;
+    private float userX, userY;
     private Path userPath;
     private Paint userPaint;
     private ArrayList <InputPath> paths = new ArrayList<>();
@@ -58,5 +59,60 @@ public class PaintView extends View {
         normal();
         invalidate();
 
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        userCanvas.drawColor(backgroundColor);
+        canvas.drawBitmap(userBitmap, 0, 0, userBitmapPaint);
+        canvas.restore();
+    }
+
+    private void inputStart(float x, float y) {
+        userPath = new Path();
+        InputPath ip = new InputPath(currentColor, strokeWidth, userPath);
+        paths.add(ip);
+
+        userPath.reset();
+        userPath.moveTo(x,y);
+        userX = x;
+        userY = y;
+    }
+
+    private void touching(float x, float y){
+        float dx = Math.abs(x-userX);
+        float dy = Math.abs(x-userY);
+
+        if( dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE){
+            userPath.quadTo(userX, userY, (x + userX) / 2, (y + userY) / 2);
+            userX = x;
+            userY = y;
+        }
+    }
+    private void notTouching() {
+        userPath.lineTo(userX, userY);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+                inputStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE :
+                touching(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP :
+                notTouching();
+                invalidate();
+                break;
+        }
+
+        return true;
     }
 }
