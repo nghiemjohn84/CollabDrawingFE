543e9a88738fbd3c5c24957fce8be9f8a7b5d560
Suneet Misra <emailsuneet@icloud.com>
2019-08-01 12:23:49 +0100

    Added connection to firebase auth database and successfully implemented login functionality for LoginActivity

--- app/src/main/java/com/example/collabdrawingfe/LoginActivity.kt
+++ app/src/main/java/com/example/collabdrawingfe/LoginActivity.kt
@@ -3,20 +3,55 @@ package com.example.collabdrawingfe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         login_button_login.setOnClickListener {
-            val userName = username_editText_login.text.toString()
+            val email = email_editText_login.text.toString()
             val password = password_editText_login.text.toString()
 
-            Log.d("LoginActivity", "username is: $userName")
-            Log.d("LoginActivity", "password is: $password")
+//            Log.d("LoginActivity", "username is: $email")
+//            Log.d("LoginActivity", "password is: $password")
+
+            signIn(email, password)
+
+
+        }
+
+
+    }
+
+    private fun signIn(email: String, password: String) {
+
+        Log.d("LoginActivity", "signIn ran")
+        Log.d("LoginActivity", "email is: $email")
+        Log.d("LoginActivity", "password is: $password")
+        Log.d("LoginActivity", mAuth.toString())
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                when{
+                    it.isSuccessful -> {
+                        Log.d("LoginActivity", "Login successful")
+                        Toast.makeText(this, "User logged in successfully", Toast.LENGTH_LONG).show()
+
+                    }
+                    else -> {
+                        Log.d("LoginActivity", "Login unsuccessful boyyyyyy :(")
+                        Toast.makeText(this, "Login failed boyyyyyyyyyyy! :(", Toast.LENGTH_LONG).show()
+
+                        return@addOnCompleteListener
+                    }
+                }
+
+
         }
     }
 }
