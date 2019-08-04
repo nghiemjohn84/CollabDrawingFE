f30770a343bd4b314fadfe915367c00ce5a8b54d
Suneet Misra <emailsuneet@icloud.com>
2019-08-04 01:24:12 +0100

    Added splash screen

---
+++ app/src/main/java/com/example/collabdrawingfe/SplashscreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.collabdrawingfe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.MainThread
+import java.lang.Exception
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                     Thread.sleep(5000)
+
+                    val intent = Intent(baseContext, LoginActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+
+    }
+}
