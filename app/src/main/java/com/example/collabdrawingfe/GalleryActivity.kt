771a72ed2e30849a2c8b7cd6b19d47b135c2d9d8
Suneet Misra <emailsuneet@icloud.com>
2019-08-05 22:18:23 +0100

    Added functionality to display images in gallery as a recycler view. This is through making use of a test database of images that must be replaced with a database of user drawings.

--- app/src/main/java/com/example/collabdrawingfe/GalleryActivity.kt
+++ app/src/main/java/com/example/collabdrawingfe/GalleryActivity.kt
@@ -2,17 +2,27 @@ package com.example.collabdrawingfe
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
 
 import kotlinx.android.synthetic.main.activity_gallery.*
+import kotlinx.android.synthetic.main.content_gallery.*
 
 class GalleryActivity : AppCompatActivity() {
+
+    private val dbFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    val drawings = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
         setSupportActionBar(toolbar)
+        fetchAllImages()
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/kotlinstorage-31352.appspot.com/o/images%2Fbart.jpg?alt=media&token=9fe1f1e0-4bae-444f-a2f7-55dc33487553").into(tempImage)
 
         newCanvas_button_gallery.setOnClickListener { view ->
             val intentActivity = Intent(this, PaintActivity::class.java)
@@ -20,6 +30,22 @@ class GalleryActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun fetchAllImages() {
+        val userRef =  dbFirestore.collection("testImages")
+        userRef.get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    var drawingURL = document.get("url")
+                    drawings.add("$drawingURL")
+                    image_list_gallery.layoutManager = LinearLayoutManager(this)
+                    image_list_gallery.adapter = GalleryRecyclerAdapter(this, drawings)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("gallery", "Error getting drawing URLs", exception)
+            }
     }
 
 }
