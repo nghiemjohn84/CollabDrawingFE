c7a0bb8a19cda1870b964c9df099eb6399d377e1
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-06 10:27:30 +0100

    created UI for joinDoodle activity

---
+++ app/src/main/java/com/example/collabdrawingfe/chooseDoodleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.collabdrawingfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chooseDoodleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_doodle)
+    }
+}
