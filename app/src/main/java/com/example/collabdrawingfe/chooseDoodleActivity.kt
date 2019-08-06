2c2249206b5ec368540957f3f99119ec46c4b86c
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-06 11:18:08 +0100

    connected chooseDoodle activity to paintActivity and passed through doodle name

--- app/src/main/java/com/example/collabdrawingfe/chooseDoodleActivity.kt
+++ app/src/main/java/com/example/collabdrawingfe/chooseDoodleActivity.kt
@@ -1,12 +1,20 @@
 package com.example.collabdrawingfe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_choose_doodle.*
 
 class chooseDoodleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_doodle)
+
+        joinDoodle_button.setOnClickListener{
+            val intent = Intent(this, PaintActivity::class.java)
+            intent.putExtra("DOODLE_NAME", doodleName_editText.text.toString())
+            startActivity(intent)
+        }
     }
 }
