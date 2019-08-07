7077fa5539b322bd1447b513d102dc16b5567f0e
paul <paul.m.dewey@gmail.com>
2019-08-07 13:55:29 +0100

    Start of sharing colours

--- app/src/main/java/com/example/collabdrawingfe/DrawInstruction.kt
+++ app/src/main/java/com/example/collabdrawingfe/DrawInstruction.kt
@@ -8,7 +8,8 @@ class DrawInstruction {
     var x: Float? = 0.0F
     var y: Float? = 0.0F
     var colour: Int? = 0
+    var strokeWidth: Int? = 0
     override fun toString(): String {
-        return "$command + $x + $y + $colour"
+        return "$command + $x + $y + $colour + $strokeWidth"
     }
 }
