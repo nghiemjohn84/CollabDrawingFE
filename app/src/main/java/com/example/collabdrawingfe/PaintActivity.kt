f3865040b12a093633ad8b32a8615feed05327d3
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-07 16:41:39 +0100

    screenshot functionality built

--- app/src/main/java/com/example/collabdrawingfe/PaintActivity.kt
+++ app/src/main/java/com/example/collabdrawingfe/PaintActivity.kt
@@ -1,23 +1,74 @@
 package com.example.collabdrawingfe
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
+import kotlin.math.log
+import kotlin.random.Random
 
 class PaintActivity : AppCompatActivity() {
 
     companion object {
         const val DOODLE_NAME = "DOODLE_NAME"
+
+        fun saveScreenshot(bitmap: Bitmap) {
+            val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val myDir = File("$path/doodles")
+            if(!myDir.exists()) {
+                myDir.mkdirs()
+            }
+            Log.d("screenshot", "$path")
+            Log.d("screenshot", "$myDir")
+
+
+            val n = Random.nextInt(10000)
+            val fileName = "image$n.jpg"
+            val file = File(myDir, fileName)
+            Log.d("screenshot", "$file")
+            if(file.exists())
+                file.delete()
+            try {
+                val out = FileOutputStream(file)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                out.flush()
+                out.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+
+
+        fun loadScreenshot(v: View, width: Int, height: Int): Bitmap {
+
+            val bit = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bit)
+//            v.layout(0, 0, v.layoutParams.width, v.layoutParams.height)
+            v.layout(0, 0, v.layoutParams.width, v.layoutParams.height)
+
+            v.draw(canvas)
+            Log.d("screenshot", "file created")
+            return bit
+        }
     }
 
     private var paintView: PaintView? = null
@@ -65,9 +116,9 @@ class PaintActivity : AppCompatActivity() {
         val metrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(metrics)
         paintView!!.init(metrics, intent.getStringExtra(DOODLE_NAME))
+
+
     }
-
-
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -157,10 +208,9 @@ class PaintActivity : AppCompatActivity() {
 
             }
             R.id.save -> {
-                val activityIntent = Intent(this, GalleryActivity::class.java)
-                //call itemtobesaved method here?
-                startActivity(activityIntent)
-
+                val bitmap = loadScreenshot(findViewById(R.id.paintView), 750, 1000)
+                saveScreenshot(bitmap!!)
+//
             }
 
         }
